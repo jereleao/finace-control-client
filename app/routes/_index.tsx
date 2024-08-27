271110b0e1b19e633e7e8d4b600f5113116094c2
Jeremias Leao <jeremaios@gmail.com>
2024-08-27 19:36:37 -0300

    add tailwind and shacn/ui

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,16 @@
-import type { MetaFunction } from "@vercel/remix";
+import type { MetaFunction } from '@vercel/remix';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <ul>
         <li>
           <a
