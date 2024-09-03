b14a435d5b4159e117d8f407bf8d2328476d1d99
Jeremias Leao <jeremaios@gmail.com>
2024-09-03 16:21:22 -0300

    correct text

--- app/routes/_app._index.tsx
+++ app/routes/_app._index.tsx
@@ -1,16 +1,7 @@
-import { Outlet, useRouteLoaderData } from '@remix-run/react';
-import { LoaderFunctionArgs, json } from '@remix-run/server-runtime';
-import { authenticator } from '@/services/auth.server';
-
-import Footer from '@/components/footer';
-import Header from '@/components/header';
-
-import { ScrollArea } from '@/components/ui/scroll-area';
-
 export default function AppIndex() {
   return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix@Edge</h1>
+    <div>
+      <h1>Welcome to Finance App</h1>
     </div>
   );
 }
--- app/routes/edge.tsx
+++ app/routes/edge.tsx
@@ -1,12 +1,8 @@
-import type { MetaFunction } from "@vercel/remix";
-
-export const config = { runtime: "edge" };
-
-export const meta: MetaFunction = () => [{ title: "Remix@Edge | New Remix App" }];
+export const config = { runtime: 'edge' };
 
 export default function Edge() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Welcome to Remix@Edge</h1>
     </div>
   );
